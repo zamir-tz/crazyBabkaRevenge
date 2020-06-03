22ec8cd5be15487d41f28070a79d202b04179cf1
z <zamir.toss@gmail.com>
2020-06-03 18:50:55 +0200

    test scene created

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontalMove = 0f;
+    private bool jump = false;
+
+    public CharacterController2D controller;
+
+
+    public float speed = 40f;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+
+       if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+
+
+}
